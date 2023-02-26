6676d2161c5fe800fc3720e064a250df1a781c41
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-02-26 07:19:10 +0200

    first commit

--- lib/view/screens/car_details/views/car_details_view.dart
+++ lib/view/screens/car_details/views/car_details_view.dart
@@ -7,28 +7,34 @@ class CarDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          physics: const BouncingScrollPhysics(),
-          padding: const EdgeInsets.only(bottom: 20),
+        child: Column(
           children: [
             const CarPageHeader(),
-            const CarDataCardRow(),
-            const PriceStateRow(),
-            const MakfolaTo(),
-            7.h,
-            const CarPageDataColumn(),
-            7.h,
-            const LoremText(),
-            15.h,
-            allCaresCard,
-            15.h,
-            const HomeGridView(
-              itemCount: 2,
-              height: 220,
-              physics: NeverScrollableScrollPhysics(),
+            Expanded(
+              child: ListView(
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.only(bottom: 20),
+                children: [
+                  const CarDataCardRow(),
+                  const PriceStateRow(),
+                  const MakfolaTo(),
+                  7.h,
+                  const CarPageDataColumn(),
+                  7.h,
+                  const LoremText(),
+                  15.h,
+                  allCaresCard,
+                  15.h,
+                  const HomeGridView(
+                    itemCount: 2,
+                    height: 220,
+                    physics: NeverScrollableScrollPhysics(),
+                  ),
+                  20.h,
+                  const CarPageIcons(),
+                ],
+              ),
             ),
-            20.h,
-            const CarPageIcons(),
           ],
         ),
       ),
